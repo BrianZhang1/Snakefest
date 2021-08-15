ddea2eae1d440880df553504d2312aed567ca359
Brian Zhang <brian22ca@gmail.com>
2021-08-15 16:55:40 -0400

    seperated app.py from game class (moved to snake/game/game.py)

--- snake/app.py
+++ snake/app.py
@@ -1,50 +1,21 @@
+import tkinter as tk
+root = tk.Tk()
+import time
+from snake.game import game as game_    # trailing _ to avoid name conflicts
+
 def run():
-    import tkinter as tk
-    root = tk.Tk()
+    # Initialize important objects
+    game = game_.Game(root)
 
-    import time
-    from snake.game import snake_control
-    from snake.game import apples
-    from snake.game import tiles
-
-    class App(tk.Frame):
-        def __init__(self, master):
-            tk.Frame.__init__(self, master)
-            self.master = master
-            self.master.bind("<Key>", self.key_handler)
-
-            # Create Canvas
-            canvas_width = root.winfo_screenwidth()
-            canvas_height = root.winfo_screenheight()
-            self.canvas = tk.Canvas(root, width=canvas_width, height=canvas_height)
-            self.canvas.pack()
-
-            # Create Objects
-            self.tiles = tiles.tiles(self.canvas)
-            self.snake = snake_control.Snake(self.canvas)
-            self.apple = apples.Apple(self.canvas)
-
-        # Input Handler
-        def key_handler(self, event):
-            if event.char == 'w':
-                self.snake.snake_direction = 'n'
-            elif event.char == 'a':
-                self.snake.snake_direction = 'w'
-            elif event.char == 's':
-                self.snake.snake_direction = 's'
-            elif event.char == 'd':
-                self.snake.snake_direction = 'e'
-
-
-    app = App(root)
+    # Event loop
     start_time = time.time()
     while True:
         if time.time() - start_time > 0.12:
             start_time = time.time()
-            app.snake.move_snake()
-            if tuple(app.snake.snake_pos) == app.apple.apple_pos:
-                app.apple.apple_pos = app.apple.rand_apple_pos()
-                app.snake.create_new_body()
+            game.snake.move_snake()
+            if tuple(game.snake.snake_pos) == game.apple.apple_pos:
+                game.apple.apple_pos = game.apple.rand_apple_pos()
+                game.snake.create_new_body()
 
         root.update_idletasks()
         root.update()
---
+++ snake/game/game.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from snake.game import snake_control
+from snake.game import apples
+from snake.game import tiles
+
+class Game(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.master = master
+        self.master.bind("<Key>", self.key_handler)
+
+        # Create Canvas
+        canvas_width = master.winfo_screenwidth()
+        canvas_height = master.winfo_screenheight()
+        self.canvas = tk.Canvas(master, width=canvas_width, height=canvas_height)
+        self.canvas.pack()
+
+        # Create Objects
+        self.tiles = tiles.tiles(self.canvas)
+        self.snake = snake_control.Snake(self.canvas)
+        self.apple = apples.Apple(self.canvas)
+
+    # Input Handler
+    def key_handler(self, event):
+        if event.char == 'w':
+            self.snake.snake_direction = 'n'
+        elif event.char == 'a':
+            self.snake.snake_direction = 'w'
+        elif event.char == 's':
+            self.snake.snake_direction = 's'
+        elif event.char == 'd':
+            self.snake.snake_direction = 'e'
