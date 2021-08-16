6d393a9e81eeb16696100a2ceb73b87940760ec5
Brian Zhang <brian22ca@gmail.com>
2021-08-16 11:56:40 -0400

    cleaned up imports

--- snake/game/apples.py
+++ snake/game/apples.py
@@ -1,28 +1,28 @@
 # apples.py creates and renders apples.
 
-from snake.game.tiles import rows, columns 
-from snake.assets import apple_sprite, rect_length, snake_head_length
+from snake.game import tiles
+from snake import assets
 import random
 
 class Apple():
-    dist_from_origin = snake_head_length/2 + (rect_length - snake_head_length)/2 + 3
+    dist_from_origin = assets.snake_head_length/2 + (assets.rect_length - assets.snake_head_length)/2 + 3
     # Image generation is center-based, so if you give the coords (2, 2) then the
     # centre is located at (2, 2). With tiles, they are top-left based. This formula
     # aligns the tiles with the apples.
     def __init__(self, canvas):
         self.canvas = canvas
-        self.apple = canvas.create_image((self.dist_from_origin, self.dist_from_origin), image=apple_sprite)
+        self.apple = canvas.create_image((self.dist_from_origin, self.dist_from_origin), image=assets.apple_sprite)
         self.apple_pos = self.rand_apple_pos()
 
     def rand_apple_pos(self):
-        x = random.randint(1, columns)
-        y = random.randint(1, rows)
+        x = random.randint(1, tiles.columns)
+        y = random.randint(1, tiles.rows)
         return self.set_apple_pos(x, y)
 
     def set_apple_pos(self, x, y):
         self.apple_pos = (x - 1, y - 1)
-        apple_x = self.dist_from_origin + self.apple_pos[0]*rect_length
-        apple_y = self.dist_from_origin + self.apple_pos[1]*rect_length
+        apple_x = self.dist_from_origin + self.apple_pos[0]*assets.rect_length
+        apple_y = self.dist_from_origin + self.apple_pos[1]*assets.rect_length
         self.canvas.coords(self.apple, (apple_x, apple_y))
         return self.apple_pos
 
--- snake/game/snake_control.py
+++ snake/game/snake_control.py
@@ -1,18 +1,17 @@
 # snake_control.py creates and controls the snake (head and body)
 # It manages movement, growth, and rendering
 
-import snake
-from snake.game.tiles import rows, columns
-from snake.assets import snake_body_sprite, snake_head_up, snake_head_down, snake_head_left, snake_head_right, rect_length, snake_head_length
+from snake.game import tiles
+from snake import assets
 
 class Snake():
-    dist_from_origin = snake_head_length/2 + (rect_length - snake_head_length)/2 + 3
+    dist_from_origin = assets.snake_head_length/2 + (assets.rect_length - assets.snake_head_length)/2 + 3
     # Image generation is center-based, so if you give the coords (2, 2) then the
     # centre is located at (2, 2). With tiles, they are top-left based. This formula
     # aligns the tiles with the snake.
     def __init__(self, canvas):
         self.canvas = canvas
-        self.snake_head = self.canvas.create_image((self.dist_from_origin, self.dist_from_origin), image=snake_head_right)
+        self.snake_head = self.canvas.create_image((self.dist_from_origin, self.dist_from_origin), image=assets.snake_head_right)
         self.snake_pos = [0, 0]         # [x, y]
         self.snake_direction = 'e'      # north, east, south, west
         self.previous_moves = []
@@ -22,30 +21,30 @@ class Snake():
         # Move one tile in the direction the snake faces
         if self.snake_direction == 'w':
             self.snake_pos[0] -= 1
-            self.canvas.itemconfig(self.snake_head, image=snake_head_left)
+            self.canvas.itemconfig(self.snake_head, image=assets.snake_head_left)
         elif self.snake_direction == 'e':
             self.snake_pos[0] += 1
-            self.canvas.itemconfig(self.snake_head, image=snake_head_right)
+            self.canvas.itemconfig(self.snake_head, image=assets.snake_head_right)
         elif self.snake_direction == 's':
             self.snake_pos[1] += 1
-            self.canvas.itemconfig(self.snake_head, image=snake_head_down)
+            self.canvas.itemconfig(self.snake_head, image=assets.snake_head_down)
         elif self.snake_direction == 'n':
             self.snake_pos[1] -= 1
-            self.canvas.itemconfig(self.snake_head, image=snake_head_up)
+            self.canvas.itemconfig(self.snake_head, image=assets.snake_head_up)
 
         # Check bounds. If out of bounds, teleport to opposite side.
         if self.snake_pos[0] < 0:
-            self.snake_pos[0] = columns - 1
-        elif self.snake_pos[0] > columns - 1:
+            self.snake_pos[0] = tiles.columns - 1
+        elif self.snake_pos[0] > tiles.columns - 1:
             self.snake_pos[0] = 0;
         elif self.snake_pos[1] < 0:
-            self.snake_pos[1] = rows - 1
-        elif self.snake_pos[1] > rows - 1:
+            self.snake_pos[1] = tiles.rows - 1
+        elif self.snake_pos[1] > tiles.rows - 1:
             self.snake_pos[1] = 0
 
         # Draw snake head
-        new_x = self.dist_from_origin + self.snake_pos[0]*rect_length
-        new_y = self.dist_from_origin + self.snake_pos[1]*rect_length
+        new_x = self.dist_from_origin + self.snake_pos[0]*assets.rect_length
+        new_y = self.dist_from_origin + self.snake_pos[1]*assets.rect_length
 
         self.canvas.coords(self.snake_head, (new_x, new_y))
         self.previous_moves.insert(0, tuple(self.snake_pos))
@@ -54,8 +53,8 @@ class Snake():
             self.previous_moves.pop()
 
         if len(self.snake_body) > 0:
-            new_coords_x = self.previous_moves[1][0]*rect_length + self.dist_from_origin
-            new_coords_y = self.previous_moves[1][1]*rect_length + self.dist_from_origin
+            new_coords_x = self.previous_moves[1][0]*assets.rect_length + self.dist_from_origin
+            new_coords_y = self.previous_moves[1][1]*assets.rect_length + self.dist_from_origin
             new_coords = (new_coords_x, new_coords_y)
 
             self.canvas.coords(self.snake_body[-1], (new_coords))
@@ -65,8 +64,8 @@ class Snake():
     def create_new_body(self):
         column = self.previous_moves[len(self.snake_body) + 1][0]
         row = self.previous_moves[len(self.snake_body) + 1][1]
-        new_coords_x = self.dist_from_origin + column*rect_length
-        new_coords_y = self.dist_from_origin + row*rect_length
+        new_coords_x = self.dist_from_origin + column*assets.rect_length
+        new_coords_y = self.dist_from_origin + row*assets.rect_length
         new_coords = (new_coords_x, new_coords_y)
-        self.snake_body.append(self.canvas.create_image(new_coords, image=snake_body_sprite))
+        self.snake_body.append(self.canvas.create_image(new_coords, image=assets.snake_body_sprite))
 
