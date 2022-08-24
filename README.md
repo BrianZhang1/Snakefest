86803803c4eba46d2c55fa3a600577b5fa61da52
Brian Zhang <brian22ca@gmail.com>
2022-08-24 15:30:37 -0400

    changed to MIT license

--- README.md
+++ README.md
@@ -41,21 +41,3 @@ Limits on maps as of 08-26:
 * Map creator preview
 * Limits for map creator fields
 * Deleting and editing maps
-
-## Copying
-
-Snakefest is an extended version of the popular Snake game.  
-Copyright (C) 2021  Brian Zhang
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
--- snake/states/game/snake.py
+++ snake/states/game/snake.py
@@ -1,19 +1,3 @@
-#    Snakefest is an extended version of the popular Snake game.
-#    Copyright (C) 2021  Brian Zhang
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>
-
 # snake_control.py creates and controls the snake (head and body)
 # It manages movement, growth, and rendering of snake.
 # It does not manage collisions of the snake with other objects, those are managed in game.py
